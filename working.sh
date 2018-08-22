47347f2f790eee985fec76d1bf2c76ba60ec8421
Anton Ryabchenko <anton.ryabchenko.92@gmail.com>
2018-08-22 21:43:34 +0300

    script list

---
+++ working.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+my_fun(){
+  i=1
+while [ $# -gt 0 ]
+  do 
+  i=$(($i*$1))
+  shift
+  done 
+  echo $i
+  }
+
+
+my_fun 20
+
+my_fun(){
+  i=0
+while [ $# -gt 0 ]
+  do 
+  i=$(($i+$1))
+  shift
+  done 
+  echo $i
+  }
+
+
+my_fun 20
+
+
+my_fun(){
+  i=1
+while [ $# -gt 0 ]
+  do 
+  i=$$(($i*$1))
+  shift
+  done 
+  echo $i
+  }
+my_fun 20
