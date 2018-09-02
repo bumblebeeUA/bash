744744196dfd10808189a2a95b5071740f9974e5
Anton Ryabchenko <anton.ryabchenko.92@gmail.com>
2018-09-03 00:51:04 +0300

    add function change_prompt

--- configure_new_box.sh
+++ configure_new_box.sh
@@ -29,3 +29,21 @@ change_hostname() {
 	hostname $1
 }
 change_hostname $1
+username=$1
+error() {
+        echo "ERROR: $1"
+}
+
+change_prompt() {
+#dev/null
+#TODO: check if PS1 already present is /home/bashrc by running grep command
+echo "export PS1=\"\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]$\"" >> /home/$1/.bashrc
+}
+
+change_prompt $username
+if [[ $? != 0 ]]
+then
+        error "couldn't change PS1 for  $username"
+#TODO: add logger in else
+fi
+
