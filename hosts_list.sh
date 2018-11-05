2cefc3ecb22a23542aeeec1c99d66eff7a6436f5
Anton Ryabchenko <anton.ryabchenko.92@gmail.com>
2018-11-05 23:49:34 +0200

    :(

--- hosts_list.sh
+++ hosts_list.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# you have to run over each host in list
-for line in `cat host_list.txt`;
-do
-  #here some function which will do all work
-  my_engine ${line}
-done
 
 my_engine(){
   line=${1}
-  number_of_columns=$(echo ${line} | awk -F "_" '{print NF}')
-
-if [ numbers_of_column = 5 ]; then
-	echo "Column equal 5"
-else
-	echo "Column doesn't equal 5"
+  host=$(echo ${line} | awk -F "_" '{print $NF}')
+#number_of_columns=$(echo ${line} | awk -F "_" '{print $NF}')
+}
+my_engine
+if [[ ${host} == ${line} ]]; then
+	echo "Column equal to 5"
+}
+#elif [[  ]]; then
+#	echo "Column doesn't equal 5"
 fi
   # here we put logic about number of columns
   # if number of columns equal to 5, then check column 5
   # if 4 column 4
   # if 3 column 3
   # else don't check just print out line and None
-}
-my_engine
+
+
+# you have to run over each host in list
+#for line in `cat host_list.txt`;
+#do
+ #here some function which will do all work
+#  my_engine ${line}
+#done
+
