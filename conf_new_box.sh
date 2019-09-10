0cf46dab5214d8a11bfd48cc508301e47bb56e56
Anton Ryabchenko <anton.ryabchenko.92@gmail.com>
2019-09-10 17:50:01 +0000

    add netplan config

--- conf_new_box.sh
+++ conf_new_box.sh
@@ -5,8 +5,8 @@
 #LIST OF VARIABLES
 ---------------------------------------------------------
 #correct link to the id_rsa.pub
-url="https://s3.amazonaws.com/arkeys/id_rsa.pub"
-ssh_dir=".ssh"
+url="https://www.dropbox.com/s/np5qgri9c56jdwu/id_rsa.pub"
+ssh_dir=".ssh/authorized_keys"
 message="helloo, you are connected to $HOST"
 ---------------------------------------------------------
 #Make update and upgrade system
@@ -17,17 +17,8 @@ else
 	echo "Updatet successfully"
 fi
 
-#It will be deprecate, cause we will install ssh in process of installation
-#Install open-ssh
-#sudo apt install ssh -y
-#if [[ "$?" ==  ]]; then
-#	echo "Something went wrong"
-#else
-#	echo "Installed successfully"
-#fi
 
 #Create .ssh dir and download ssh key from S3
-# ^^ bullshit
 # need to use this scp ~/.ssh/id_rsa.pub user@192.168.42.119:~/.ssh/authorized_keys - root required
 
 mkdir $HOME/$ssh_dir && wget ${url} -P $HOME/$ssh_dir
@@ -38,7 +29,6 @@ else
 fi
 
 #Change MOTD
-#Run only if you are root - must be reworked
 sudo sh -c "echo $message> /etc/motd"
 if [[ "$?" == 1 ]]; then
         echo "Something went wrong"
@@ -58,9 +48,36 @@ fi
 #Reconfigure sshd_conf, remove authentiation by password
 #don't work
 #should use sudo sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication no/g" sshd_config but root required
+#TO-DO: add restart ssh
 cd /etc/ssh && cat sshd_config | grep ^PasswordAuthentication | awk '{print $2}' && sed -i 's/yes/no/g' sshd_config
 
 #Set static ipv4 address
+# Creates a backup
+cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bk_`date +%Y%m%d%H%M`
+# Changes dhcp from 'yes' to 'no'
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/50-cloud-init.yaml
+# Retrieves the NIC information
+nic=`ifconfig | awk 'NR==1{print $1}'`
+# Ask for input on network configuration
+echo "Notice: write IP format is ip/mask"
+read -p "Enter the static IP of the server in CIDR notation: " staticip
+read -p "Enter the IP of your gateway: " gatewayip
+read -p "Enter the IP of preferred nameservers (seperated by a coma if more than one): " nameserversip
+echo
+cat > /etc/netplan/50-cloud-init.yaml <<EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $nic
+      dhcp4: no
+      dhcp6: no
+      addresses: [$staticip]
+      gateway4: $gatewayip
+      nameservers:
+        addresses: [$nameserverip]
+EOF
+sudo netplan apply
 
 #Echo message with successful or error status
 
